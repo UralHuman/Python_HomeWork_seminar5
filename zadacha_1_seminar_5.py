84fbf2acf5f28896b2fa9b6492c88b77c321f44e
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-03-27 22:29:20 +0500

    create_zadacha_1.1

--- zadacha_1_seminar_5.py
+++ zadacha_1_seminar_5.py
@@ -1,12 +1,26 @@
+# def power(base, exponent):
+#     if exponent == 0:
+#         return 1
+#     elif exponent % 2 == 0:
+#         result = power(base, exponent/2)
+#         return result * result
+#     result = power(base, (exponent-1)/2)
+#     return base * result * result
+    
+# a = int(input('Введите число А: '))
+# b = int(input('Введите число B: '))
+
+# print(f'{a} в степени {b} равно {power(a, b)}')
+
 def power(base, exponent):
     if exponent == 0:
         return 1
-    elif exponent % 2 == 0:
-        result = power(base, exponent/2)
-        return result * result
-    result = power(base, (exponent-1)/2)
-    return base * result * result
-    
+    else:
+        if exponent % 2 == 0:
+            return power(base, exponent/2) * power(base, exponent/2)
+        else:
+            return power(base, (exponent-1)/2) * power(base, (exponent-1)/2) * base
+        
 a = int(input('Введите число А: '))
 b = int(input('Введите число B: '))
 
